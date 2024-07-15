42ac46dcf3e23239da1763f837867b9d167c9241
laural87 <laura.lpzvgs@gmail.com>
2024-07-15 10:16:33 +0200

    upload until ex 10

--- bda-modulo-2-evaluacion-final-laulal87.sql
+++ bda-modulo-2-evaluacion-final-laulal87.sql
@@ -4,28 +4,64 @@ USE Sakila;
 -- 1. Selecciona todos los nombres de las películas sin que aparezcan duplicados.
 
 SELECT DISTINCT title
-FROM film;
+	FROM film;
 
 -- 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
 
 Select title
-FROM film
-WHERE rating = 'PG-13';
+	FROM film
+	WHERE rating = 'PG-13';
 
 -- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
 
 SELECT title , description
-FROM film
-WHERE description LIKE '%amazing%';
+	FROM film
+	WHERE description LIKE '%amazing%';
 
 -- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
 
 SELECT title, length
-FROM film
-WHERE length > 120;
+	FROM film
+	WHERE length > 120;
 
 -- 5. Recupera los nombres de todos los actores.
 
 SELECT first_name, last_name
-FROM actor;
+	FROM actor;
+
+-- 6. Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido.
+
+SELECT first_name, last_name
+	FROM actor
+	WHERE last_name = 'Gibson';
+
+-- 7. Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20.
+
+SELECT first_name, last_name
+	FROM actor
+    WHERE actor_id >=10 AND actor_id <= 20;	
+    
+-- 8. Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación.
+
+Select title, rating
+	FROM film
+	WHERE rating NOT IN ('R', 'PG-13');
+    
+-- 9. Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.
+
+SELECT COUNT(film_id) AS cantidad_peliculas, rating
+	FROM film
+    GROUP BY rating;
+    
+-- 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
+
+SELECT customer.customer_id, customer.first_name, customer.last_name, COUNT(rental.rental_id) AS Total_Alquiladas
+FROM customer
+INNER JOIN rental
+ON customer.customer_id = rental.customer_id
+GROUP BY customer.customer_id, customer.first_name, customer.last_name;
+
+-- 11. 
+
+
 
